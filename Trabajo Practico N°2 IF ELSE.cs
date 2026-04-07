67586f031d6fb7386cc0fda5f928eb1fd200e898
Lautaro-Pereyra937 <lautaro.pereyraaaet32@gmail.com>
2026-04-07 12:50:51 -0300

    Add files via upload

---
+++ Trabajo Practico N°2 IF ELSE.cs
@@ -0,0 +1,277 @@
+﻿int b1 = 25;
+int b2 = 30;
+
+int b3 = 14;
+int b4 = 14;
+
+int b5 = 0;
+int b6 = 0;
+
+int b7 = 10;
+int b8 = 10;
+
+int b9 = 8;
+int b10 = 3;
+
+int b11 = 15;
+int b12 = 15;
+
+int b13 = 100;
+int b14 = 50;
+
+int b15 = 7;
+int b16 = 7;
+
+int b17 = 5;
+int b18 = 0;
+
+int b19 = -5;
+int b20 = -2;
+
+int c1 = 5;
+int c2 = 3;
+
+int c3 = 10;
+int c4 = 20;
+
+int c5 = 8;
+int c6 = 10;
+
+int c7 = 5;
+int c8 = 2;
+
+int c9 = 10;
+int c10 = 10;
+
+int c11 = 15;
+int c12 = 10;
+
+int c13 = 5;
+int c14 = 10;
+
+int c15 = 10;
+int c16 = 2;
+int c17 = 10;
+
+int c18 = 5;
+int c19 = 2;
+int c20 = 8;
+
+int c21 = 35;
+int c22 = 30;
+bool humedad = false;
+
+int c23 = 16;
+int c24 = 18;
+
+int c25 = 10;
+int c26 = 3;
+int c27 = 3;
+
+int c28 = 10;
+int c29 = 3;
+int c30 = 3;
+
+string nombre = "Ana";
+string nombre1 = "Juan";
+
+int c31 = 5;
+int c32 = 2;
+int c33 = 10;
+int c34 = 15;
+int c35 = 20;
+int c36 = 4;
+int c37 = 5;
+
+if (b1 > b2)
+{
+    Console.WriteLine("Es verdadero");
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b3 >= b4)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b5 == b6)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b7 != b8)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b9 >= b10)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b11 < b12)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b13 != b14)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b15 == b16)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b17 >= b18)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (b19 < b20)
+{
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+
+Console.WriteLine("____________");
+
+if (c1 > c2 && c3 < c4)
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (c5 == c6 || c7 != c8)
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (!(c9 == c10))
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (c11 > c12 && !(c13 > c14))
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (c15 + c16 > c17 || c18 * c19 < c20)
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (c21 > c22 && humedad == true)
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if !(c23 >= c24)
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (c25 / c26 == c27 && c28 / c29 == c30)
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if (nombre == "Ana" || nombre1 == "Juan")
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+if ((c31 * c32) + c33 > c34 && c35 / c36 == c37)
+{
+
+    Console.WriteLine("Es verdadero");
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
