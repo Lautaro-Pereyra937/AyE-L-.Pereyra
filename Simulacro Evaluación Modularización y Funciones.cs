6738a7b7c8d07e5aa816668a5008dccaddfcdee9
Lautaro-Pereyra937 <lautaro.pereyraaaet32@gmail.com>
2026-04-07 12:59:31 -0300

    Add files via upload

---
+++ Simulacro Evaluación Modularización y Funciones.cs
@@ -0,0 +1,28 @@
+﻿void prueba()
+{
+    Console.WriteLine("ingrese su nombre");
+    string nombre = Console.ReadLine();
+    Console.WriteLine("Ingrese su promedio");
+    float promedio = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese la distancia de su casa a la universidad");
+    int distancia = Convert.ToInt32(Console.ReadLine());
+
+    static bool DeterminarBeca(float promedio, int distancia)
+    {
+        bool resultado = DeterminarBeca(promedio, distancia);
+        if (resultado == true)
+        {
+            Console.Write("Felicidades ");
+            Console.Write(nombre);
+            Console.Write(", ");
+            Console.WriteLine("tu beca ha sido aprobada");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Lo sentimos, no cumples con los requisitos mínimos");
+            return false;
+        }
+    }
+}
+prueba();
