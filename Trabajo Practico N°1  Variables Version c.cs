657711bcea9a8dc380e4ef09f5809717c21e2420
Lautaro-Pereyra937 <lautaro.pereyraaaet32@gmail.com>
2026-04-07 12:31:19 -0300

    Add files via upload

---
+++ Trabajo Practico N°1  Variables Version c.cs
@@ -0,0 +1,23 @@
+﻿A1 = 12 + 8 * 3 = 36;
+Console.WriteLine(A1);
+
+A2 = (12 + 8) * 3 = 60;
+Console.WriteLine(A2)
+
+A3 = 50 / 5 + 4 = 14;
+Console.WriteLine(A3)
+
+A4 = 100 - 10 * 2 = 80;
+Console.WriteLine(A4)
+
+A5 = 15 /%2 = 1;
+Console.WriteLine(A5)
+
+A6 = 20 /% 5 = 0;
+Console.WriteLine(A6)
+
+A8 = 10 / 2 * 4 = 20;
+Console.WriteLine(A8)
+
+A10 = 5 + 5 * 10 - 2 = 53;
+Console.WriteLine(A10)
