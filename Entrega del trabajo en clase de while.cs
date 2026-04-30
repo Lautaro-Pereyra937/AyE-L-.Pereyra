6f00180d8a88bfbbad46ce577053d23fe1f9fcdd
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-04-30 09:04:54 -0300

    Add files via upload

---
+++ Entrega del trabajo en clase de while.cs
@@ -0,0 +1,39 @@
+﻿void ejercicio1()
+{
+    bool incorrecto = false;
+    Console.WriteLine("Ingrese 0 o 1");
+
+    while (incorrecto == false)
+    {
+        int numero = Convert.ToInt32(Console.ReadLine());
+        if (numero != 1 || numero != 0)
+        {
+            Console.WriteLine("Ingrese 0 o 1");
+        }
+        else
+        {
+            Console.WriteLine("bien");
+            incorrecto = true;
+        }
+    }
+}
+
+void ejercicio2()
+{
+    bool incorrecto2 = false;
+    Console.WriteLine("Ingrese un numero de dos cifras");
+
+    while (incorrecto2 == false)
+    {
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+        if (numero2 < 10 || numero2 > 99)
+        {
+            Console.WriteLine("Ingrese un numero de dos cifras");
+        }
+        else
+        {
+            Console.WriteLine("bien");
+            incorrecto2 = true;
+        }
+    }
+}
