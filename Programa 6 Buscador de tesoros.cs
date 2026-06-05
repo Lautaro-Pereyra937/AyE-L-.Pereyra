c8f6fdf251716b15ffc08ccc9f3a035185522a31
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-06-05 20:59:41 -0300

    Update Programa 6 Buscador de tesoros.cs

--- Programa 6 Buscador de tesoros.cs
+++ Programa 6 Buscador de tesoros.cs
@@ -1,126 +1,135 @@
-﻿void programa6()
+try
 {
-    int[,] matriz1 = new int[10, 10];
-
-    int unos = 0;
-
-    int intentos = 5;
-
-    bool adivinar = true;
-
-    int adivinados = 0;
-
-    int ultimaX = -1;
-    int ultimaY = -1;
-
-    Random num = new Random();
-
-    while(unos < 3)
+    void programa6()
     {
-        int fila = num.Next(0, 10);
-        int columna = num.Next(0, 10);
-
-        if(matriz1[fila, columna] == 0)
-        {
-            matriz1[fila, columna] = 1;
-            unos++;
-        }
-    }
-
-    for (int fila = 0; fila < matriz1.GetLength(0); fila++)
-    {
-        for (int columna = 0; columna < matriz1.GetLength(1); columna++)
-        {
-            Console.Write("X" + " ");
-        }
-        Console.WriteLine();
-    }
-
-    while(adivinar == true)
-    {
-        Console.WriteLine("Elija la casilla en la cordenada x del 1 al 10");
-        int corx = Convert.ToInt32(Console.ReadLine())-1;
-
-        Console.WriteLine("Elija la casilla en la cordenada y del 1 al 10(ya ingreso la cordenada en x)");
-        int cory = Convert.ToInt32(Console.ReadLine())-1;
-
-        ultimaX = corx;
-        ultimaY = cory;
-
         string[,] matrizVisual = new string[10, 10];
-
-        if (matriz1[corx, cory] == 1)
+        for (int fila = 0; fila < matrizVisual.GetLength(0); fila++)
         {
-            Console.WriteLine("Usted acerto");
-            adivinados++;
-
-            for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+            for (int columna = 0; columna < matrizVisual.GetLength(1); columna++)
             {
-                for (int columna = 0; columna < matriz1.GetLength(1); columna++)
-                {
-                    if(fila == ultimaX && columna == ultimaY)
-                    {
-                        Console.Write(matriz1[fila, columna] + " ");
-                    }
-                    else
-                    {
-                        Console.WriteLine("X ");
-                    }
-                }
-                Console.WriteLine();
-            }
-            if (adivinados == 3)
-            {
-                Console.WriteLine("Usted gano el juego Acerto Todas las posiciones, la matriz usada fue: ");
-
-                for (int fila = 0; fila < matriz1.GetLength(0); fila++)
-                {
-                    for (int columna = 0; columna < matriz1.GetLength(1); columna++)
-                    {
-                        Console.WriteLine(matriz1[fila, columna] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                adivinar = false;
-                break;
+                matrizVisual[fila,columna] = "?" + " ";
             }
         }
-        else
+         
+        int[,] matriz1 = new int[10, 10];
+    
+        int unos = 0;
+    
+        int intentos = 5;
+    
+        bool adivinar = true;
+    
+        int adivinados = 0;
+    
+        int ultimaX = -1;
+        int ultimaY = -1;
+    
+        Random num = new Random();
+    
+        while(unos < 3)
         {
-            intentos--;
-            Console.WriteLine($"Usted no acerto tiene {intentos} intentos");
-
-            for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+            int fila = num.Next(0, 10);
+            int columna = num.Next(0, 10);
+    
+            if(matriz1[fila, columna] == 0)
             {
-                for (int columna = 0; columna < matriz1.GetLength(1); columna++)
-                {
-                    if(fila == ultimaX && columna == ultimaY)
-                    {
-                        Console.Write(matriz1[fila, columna] + " ");
-                    }
-                    else
-                    {
-                        Console.Write("X ");
-                    }
-                }
-                Console.WriteLine();
+                matriz1[fila, columna] = 1;
+                unos++;
             }
-            if (intentos == 0)
+        }
+    
+        for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+        {
+            for (int columna = 0; columna < matriz1.GetLength(1); columna++)
             {
-                Console.WriteLine("Perdio, las casillas eran: ");
-
-                for (int fila = 0; fila < matriz1.GetLength(0); fila++)
-                {
-                    for (int columna = 0; columna < matriz1.GetLength(1); columna++)
+                Console.Write("?" + " ");
+            }
+            Console.WriteLine();
+        }
+    
+        while(adivinar == true)
+        {
+            Console.WriteLine("Elija la casilla en la cordenada x del 1 al 10");
+            int corx = Convert.ToInt32(Console.ReadLine())-1;
+    
+            Console.WriteLine("Elija la casilla en la cordenada y del 1 al 10(ya ingreso la cordenada en x)");
+            int cory = Convert.ToInt32(Console.ReadLine())-1;
+    
+            ultimaX = corx;
+            ultimaY = cory;
+    
+            if(matriz1[corx, cory] == 1)
+            {
+                Console.WriteLine("Usted acerto");
+                adivinados++;
+    
+                matrizVisual[corx, cory] = "O";
+    
+                 for (int fila = 0; fila < matrizVisual.GetLength(0); fila++)
                     {
-                        Console.Write(matriz1[fila, columna] + " ");
+                        for (int columna = 0; columna < matrizVisual.GetLength(1); columna++)
+                        {
+                            Console.Write(matrizVisual[fila, columna] + " ");
+                        }
+                        Console.WriteLine();
                     }
-                    Console.WriteLine();
+                
+                if (adivinados == 3)
+                {
+                    Console.WriteLine("Usted gano el juego Acerto Todas las posiciones, la matriz usada fue: ");
+    
+                    for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+                    {
+                        for (int columna = 0; columna < matriz1.GetLength(1); columna++)
+                        {
+                            Console.WriteLine(matriz1[fila, columna] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    adivinar = false;
+                    break;
                 }
-                adivinar = false;
-                break;
+            }
+            else
+            {
+                intentos--;
+                Console.WriteLine($"Usted no acerto tiene {intentos} intentos");
+    
+                matrizVisual[corx, cory] = "X";
+                
+                for (int fila = 0; fila < matrizVisual.GetLength(0); fila++)
+                    {
+                        for (int columna = 0; columna < matrizVisual.GetLength(1); columna++)
+                        {
+                            Console.Write(matrizVisual[fila, columna] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                if (intentos == 0)
+                {
+                    Console.WriteLine("Perdio, las casillas eran: ");
+    
+                    for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+                    {
+                        for (int columna = 0; columna < matriz1.GetLength(1); columna++)
+                        {
+                            Console.Write(matriz1[fila, columna] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    adivinar = false;
+                    break;
+                }
             }
         }
     }
+    programa6();
 }
-programa6();
+catch(System.FormatException)
+{
+  Console.WriteLine("No puede");
+}
+catch(IndexOutOfRangeException)
+{
+  Console.WriteLine("No puede");
+}
