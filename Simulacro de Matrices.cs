095cccfdbbff5da92370d5a02a8cdcd9d9d9818b
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-06-16 21:47:51 -0300

    Update Simulacro de Matrices.cs

--- Simulacro de Matrices.cs
+++ Simulacro de Matrices.cs
@@ -2,7 +2,11 @@
 
 string[,] equipo2 = new string[23, 3];
 
-string[] nombres = ["jorge", "messi", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge", "jorge",];
+string[] nombres = [ "Romario", "Bergkamp", "Zidane", "Simeone", "Kaka",
+    "Maldini", "Totti", "Ronaldo Fenomeno", "Gattuso", "Iniesta",
+    "Xavi", "Schweinsteiger", "Pirlo", "Buffon", "Cannavaro",
+    "Ronaldo Nazario", "Ronaldinho", "Kluivert", "Dugarry", "Veron",
+    "Scolari", "Van Nistelrooy", "Maldini"];
 
 string[] posicion = ["delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero", "delantero", "mediocampista", "defensa", "arquero"];
 
@@ -13,73 +17,85 @@ Random aleatorio = new Random();
 int puntuacionequipo1 = 0;
 
 int puntuacionequipo2 = 0;
-void ejercicio1()
+void loop()
 {
+    crearequipo1(equipo1);
+    Console.WriteLine("Real Mandril:");
+    mostrarequipo1(equipo1);
 
-    for (int fila = 0; fila < equipo1.GetLength(0); fila++)
-    {
-        int rendi = rendimiento.Next(50, 101);
-
-        equipo1[fila, 0] = nombres[aleatorio.Next(1, 24)];
-
-        equipo1[fila, 1] = posicion[aleatorio.Next(1, 4)];
-
-        equipo1[fila, 2] = aleatorio.Next(50, 101).ToString();
-
-        puntuacionequipo1 += rendi;
-
-        Console.WriteLine();
-    }
-
-    for(int fil  = 0;fil< equipo1.GetLength(0); fil++)
-    {
-        Console.WriteLine(equipo1[fil, 0], equipo1[fil, 0], equipo1[fil, 2], " ");
-    }
-
+    crearequipo2(equipo2);
+    Console.WriteLine("Frosta Juniors");
+    mostrarequipo2(equipo2);
+    
     puntuacionequipo1 = puntuacionequipo1 / 23;
-    Console.WriteLine($"La puntuacion total del equipo es: {puntuacionequipo1}");
+    puntuacionequipo2= puntuacionequipo2 / 23;
+
+    ganador(puntuacionequipo1,puntuacionequipo2);
 }
-ejercicio1();
+loop();
 
 string[,] crearequipo1(string[,] equipo)
 {
-    for (int fila = 0; fila < equipo1.GetLength(0); fila++)
+    for (int fila = 0; fila < equipo.GetLength(0); fila++)
     {
         int rendi = rendimiento.Next(50, 101);
 
-        equipo1[fila, 0] = nombres[aleatorio.Next(1, 24)];
+        equipo[fila, 0] = nombres[aleatorio.Next(0, nombres.Length)];
 
-        equipo1[fila, 1] = posicion[aleatorio.Next(1, 4)];
+        equipo[fila, 1] = posicion[aleatorio.Next(0, posicion.Length)];
 
-        equipo1[fila, 2] = aleatorio.Next(50, 101).ToString();
+        equipo[fila, 2] = rendi.ToString();
 
         puntuacionequipo1 += rendi;
     }
-    return equipo1;
+    return equipo;
 }
 
-void mostrarequipo1()
+void mostrarequipo1(string equipo)
 {
-    for()
+    for(int fil  = 0;fil< equipo.GetLength(0); fil++)
+    {
+        Console.WriteLine(equipo[fil, 0], equipo[fil, 1], equipo[fil, 2], " ");
+    }
 }
+
 string[,] crearequipo2(string[,] equipo)
 {
-    for (int fila = 0; fila < equipo2.GetLength(0); fila++)
+    for (int fila = 0; fila < equipo.GetLength(0); fila++)
     {
         int rendi = rendimiento.Next(50, 101);
 
-        equipo2[fila, 0] = nombres[aleatorio.Next(1, 24)];
+        equipo[fila, 0] = nombres[aleatorio.Next(0, nombres.Length)];
 
-        equipo2[fila, 1] = posicion[aleatorio.Next(1, 4)];
+        equipo[fila, 1] = posicion[aleatorio.Next(0, posicion.Length)];
 
-        equipo2[fila, 2] = aleatorio.Next(50, 101).ToString();
+        equipo[fila, 2] = rendi.ToString();
 
         puntuacionequipo2 += rendi;
     }
-    return equipo2;
+    return equipo;
 }
 
-void mostrarequipo2()
+void mostrarequipo2(string equipo)
 {
+    for(int fil  = 0;fil< equipo.GetLength(0); fil++)
+    {
+        Console.WriteLine(equipo[fil, 0], equipo[fil, 1], equipo[fil, 2], " ");
+    }
+}
 
+void ganador(int puntuacionequipo2, int puntuacionequipo1)
+{
+   if(puntuacionequipo2 > puntuacionequipo1)
+   {
+      Console.WriteLine($"El ganador es el equipo 2 con {puntuacionequipo2} de puntuacion promedio");
+   }
+   else if(puntuacionequipo2 < puntuacionequipo1)
+   {
+     Console.WriteLine($"El ganador es el equipo 1 con {puntuacionequipo1} de puntuacion promedio");
+   }
+   else if(puntuacionequipo2 == puntuacionequipo1)
+   {
+    Console.WriteLine($"Han empatado con una puntuacion de {puntuacionequipo1}");
+   }
 }
