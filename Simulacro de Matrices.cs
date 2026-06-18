57dc7860fb45ee2ba805c18ba6500e939899e2da
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-06-18 08:56:03 -0300

    Update Simulacro de Matrices.cs

--- Simulacro de Matrices.cs
+++ Simulacro de Matrices.cs
@@ -1,4 +1,4 @@
-﻿string[,] equipo1 = new string[23, 3];
+string[,] equipo1 = new string[23, 3];
 
 string[,] equipo2 = new string[23, 3];
 
@@ -23,14 +23,16 @@ void loop()
     Console.WriteLine("Real Mandril:");
     mostrarequipo1(equipo1);
 
+    Console.WriteLine("------");
+
     crearequipo2(equipo2);
     Console.WriteLine("Frosta Juniors");
     mostrarequipo2(equipo2);
-    
-    puntuacionequipo1 = puntuacionequipo1 / 23;
-    puntuacionequipo2= puntuacionequipo2 / 23;
 
-    ganador(puntuacionequipo1,puntuacionequipo2);
+    puntuacionequipo1 = puntuacionequipo1 / 23;
+    puntuacionequipo2 = puntuacionequipo2 / 23;
+
+    ganador(puntuacionequipo2, puntuacionequipo1);
 }
 loop();
 
@@ -40,22 +42,22 @@ string[,] crearequipo1(string[,] equipo)
     {
         int rendi = rendimiento.Next(50, 101);
 
+        puntuacionequipo1 += rendi;
+
         equipo[fila, 0] = nombres[aleatorio.Next(0, nombres.Length)];
 
         equipo[fila, 1] = posicion[aleatorio.Next(0, posicion.Length)];
 
         equipo[fila, 2] = rendi.ToString();
-
-        puntuacionequipo1 += rendi;
     }
     return equipo;
 }
 
-void mostrarequipo1(string equipo)
+void mostrarequipo1(string[,] equipo)
 {
-    for(int fil  = 0;fil< equipo.GetLength(0); fil++)
+    for (int fil = 0; fil < equipo.GetLength(0); fil++)
     {
-        Console.WriteLine(equipo[fil, 0], equipo[fil, 1], equipo[fil, 2], " ");
+        Console.WriteLine($"{equipo[fil, 0]} - {equipo[fil, 1]} - {equipo[fil, 2]}"+ " ");
     }
 }
 
@@ -76,28 +78,28 @@ string[,] crearequipo2(string[,] equipo)
     return equipo;
 }
 
-void mostrarequipo2(string equipo)
+void mostrarequipo2(string[,] equipo)
 {
-    for(int fil  = 0;fil< equipo.GetLength(0); fil++)
+    for (int fil = 0; fil < equipo.GetLength(0); fil++)
     {
-        Console.WriteLine(equipo[fil, 0], equipo[fil, 1], equipo[fil, 2], " ");
+        Console.WriteLine($"{equipo[fil, 0]} - {equipo[fil, 1]} - {equipo[fil, 2]}"+ " ");
     }
 }
 
 void ganador(int puntuacionequipo2, int puntuacionequipo1)
 {
-   if(puntuacionequipo2 > puntuacionequipo1)
-   {
-      Console.WriteLine($"El ganador es el equipo 2 con {puntuacionequipo2} de puntuacion promedio");
-   }
-   else if(puntuacionequipo2 < puntuacionequipo1)
-   {
-     Console.WriteLine($"El ganador es el equipo 1 con {puntuacionequipo1} de puntuacion promedio");
-   }
-   else if(puntuacionequipo2 == puntuacionequipo1)
-   {
-    Console.WriteLine($"Han empatado con una puntuacion de {puntuacionequipo1}");
-   }
+    if (puntuacionequipo2 > puntuacionequipo1)
+    {
+        Console.WriteLine($"El ganador es el equipo 2 con {puntuacionequipo2} de puntuacion promedio");
+    }
+    else if (puntuacionequipo2 < puntuacionequipo1)
+    {
+        Console.WriteLine($"El ganador es el equipo 1 con {puntuacionequipo1} de puntuacion promedio");
+    }
+    else if (puntuacionequipo2 == puntuacionequipo1)
+    {
+        Console.WriteLine($"Han empatado con una puntuacion de {puntuacionequipo1}");
+    }
 }
 
 Console.WriteLine("Ingrese el numero a elevar");
@@ -106,14 +108,14 @@ int elevar = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ingrese su potencia");
 int potencia = Convert.ToInt32(Console.ReadLine());
 
-int potenciadeunnumero(int elevar,int potencia)
-{    
-   if(potencia == 0)
-   {
-      return 1;
-   }
-   else
-   {
-      return elevar*potenciadeunnumero(elevar,potencia-1);
-   }
+int potenciadeunnumero(int elevar, int potencia)
+{
+    if (potencia == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return elevar * potenciadeunnumero(elevar, potencia - 1);
+    }
 }
