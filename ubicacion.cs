2655876065d9fb2dcd1089eb99e372196810ba28
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-09-01 11:34:55 -0300

    Add files via upload

---
+++ ubicacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public struct ubicacion
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public string nombre_zona { get; set; }
+
+        public ubicacion(int x, int y, string nombre_zona)
+        {
+            this.x = x;
+            this.y = y;
+            this.nombre_zona = nombre_zona;
+        }
+    }
+}
