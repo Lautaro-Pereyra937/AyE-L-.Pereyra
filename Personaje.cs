5f386d1415353f3b34d76d2a3d06c7bc6a0ac857
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-09-03 10:20:07 -0300

    Add files via upload

---
+++ Personaje.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public struct Personaje
+    {
+        public int vidatotal { get; set; }
+        public int vidaactual { get; set; }
+        public string ultimaaccion { get; set; }
+
+        public Stack<Personaje> historialDelPersonaje = new Stack<Personaje>();
+
+        public Personaje(int vidatotal, int vidaactual, string ultimaaccion)
+        {
+            this.vidatotal = vidatotal;
+            this.vidaactual = vidaactual;
+            this.ultimaaccion = ultimaaccion;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Personaje> historialDelPersonaje = new Stack<Personaje>();
+
+            Personaje accion1 = new Personaje(100, 100, "Volar");
+            historialDelPersonaje.Push(accion1);
+
+            Personaje accion2 = new Personaje(100, 77, "Golpear");
+            historialDelPersonaje.Push(accion2);
+
+            Personaje accion3 = new Personaje(100, 14, "Tomar");
+            historialDelPersonaje.Push(accion3);
+
+            foreach (Personaje acciones in historialDelPersonaje)
+            {
+                Console.WriteLine($"Todos los cambios almacenados en el historial: Vida total: {acciones.vidatotal} Vida actual: {acciones.vidaactual} Accion: {acciones.ultimaaccion}");
+            }
+
+            if (historialDelPersonaje.TryPop(out Personaje personajeactual))
+            {
+                Console.WriteLine($"Ultima accion borrada: {personajeactual.ultimaaccion}");
+            }
+            else
+            {
+                Console.WriteLine("No hay acciones para borrar");
+            }
+            int dañogolpe = 20;
+
+            Personaje accion4 = new Personaje(100,68, "Golpe recibido");
+            historialDelPersonaje.Push(accion4);
+
+            Console.WriteLine($"vida total: {accion4.vidatotal} Vida actual: {accion4.vidaactual-dañogolpe} Accion: {accion4.ultimaaccion}");
+
+        }
+    }
+}
